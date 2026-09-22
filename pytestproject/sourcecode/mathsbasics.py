59e4b7f5e5f88d5320adcc554b0123abbc57bc2f
pgudi <prabhakargudi@gmail.com>
2026-09-22 19:56:04 +0530

    Pytest Unit Testing Framework

---
+++ pytestproject/sourcecode/mathsbasics.py
@@ -0,0 +1,12 @@
+
+def addition(num1,num2):
+    result=(num1 + num2)
+    return result
+
+def multiplication(num1, num2):
+    result=(num1 * num2)
+    return result
+
+def substraction(num1, num2):
+    result=(num1 - num2)
+    return result
---
+++ pytestproject/tests/test_customers.py
@@ -0,0 +1,9 @@
+class TestCustomers:
+    def test_create_customer(self):
+        print("The Customer demoCustomer has created successfully")
+
+    def test_modify_customer(self):
+        print("The Customer demoCustomer has modified successfully")
+
+    def test_delete_customer(self):
+        print("The Customer demoCustomer has deleted successfully")
